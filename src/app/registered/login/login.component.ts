4e79e25cc2f213a6638e314fd3ee265c51e6f835
zcl739588122 <739588122@qq.com>
2020-05-20 16:57:15 +0800

    service and api

--- src/app/registered/login/login.component.ts
+++ src/app/registered/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router} from '@angular/router';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -10,39 +10,36 @@ import { Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   validateForm: FormGroup;
-  phone:'';
-  Tip:any = '请输入登录手机号码';
-  pasd:any = '请输入密码';
-  constructor(private fb: FormBuilder,private router: Router) {}
+  phone: string;
+  password: string;
+  Tip: any = '请输入登录手机号码';
+  pasd: any = '请输入密码';
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router) {
+    }
 
   ngOnInit(): void {
-    this.Tip='请输入11位手机号码';
+    this.Tip = '请输入11位手机号码';
     this.pasd = '请输入密码';
     this.validateForm = this.fb.group({
-      userName: [null, [Validators.required,Validators.minLength(11),Validators.pattern(new RegExp(/^1[3456789]\d{9}$/),)]],
-      password: [null, [Validators.required,Validators.minLength(6),]],
-      remember: [true]
+      userName: [null, [Validators.required, Validators.minLength(11), Validators.pattern(new RegExp(/^1[3456789]\d{9}$/))]],
+      password: [null, [Validators.required, Validators.minLength(6)]],
     });
   }
 
   submitForm(): void {
-    for (const i in this.validateForm.controls) {
-      this.Tip = '请输入你的登录号码';
-      this.validateForm.controls[i].markAsDirty();
-      this.validateForm.controls[i].updateValueAndValidity();
-    }
-    console.log('this.validateForm.errors',this.validateForm.valid)
-    if(this.validateForm.valid){
-        this.router.navigate([ '/park'])
+    if (this.validateForm.valid) {
+      console.log('1111', this.validateForm.value);
     }
   }
 
-  Tips():void {
-    this.Tip = '请输入11位手机号码'
+  Tips(): void {
+    this.Tip = '请输入11位手机号码';
   }
-  Psws():void {
-    this.pasd = '至少6位字符'
-  }
-  
 
+  Psws(): void {
+    this.pasd = '至少6位字符';
+  }
 }
