f1feb34a9df13c52aefd4b4fb10b59e968c4d553
zcl739588122 <739588122@qq.com>
2020-05-20 17:19:05 +0800

    new file

---
+++ src/app/service/core.api.ts
@@ -0,0 +1,7 @@
+export const ApiAddress = {
+    LOGIN: 'login',
+    REGISTRER: 'register',
+    USER_EXISTENCE: 'check_username_unique/:username',
+    CAPTCHA_SMS: 'vaild_code', // 获取手机验证码 & 校验图形验证码
+    EDIT_PASSWORD: 'edit_password',
+};
